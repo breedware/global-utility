16b9120886a47d16c996f7fef46dfe5d82f1ff5e
breedware limited <breedware@gmail.com>
2026-04-24 10:02:09 +0100

    json file type corrected

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 
 export * from './types/messaging.types.js';
 export * from './utils/index.js';
-export { default as countries } from './constant/countries.json';
+// export { default as countries } from './constant/countries.json';
+// in your @breedware/global-utility source file
+export { default as countries } from "./constant/countries.json" with { type: "json" };
