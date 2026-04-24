e5da09ed991c580c2009742478ceda70d81869c2
breedware limited <breedware@gmail.com>
2026-04-24 11:08:21 +0100

    remove json file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 
 export * from './types/messaging.types.js';
 export * from './utils/index.js';
-// export { default as countries } from './constant/countries.json';
-// in your @breedware/global-utility source file
-export { default as countries } from "./constant/countries.json" with { type: "json" };
